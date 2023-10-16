c42b4e68f0123ed32bf5d2e235f9a6302d8e7361
AdairPonceuwu <adair.ponce@alumno.buap.mx>
2023-10-16 00:13:30 -0600

    Agregando video, correccion de dashboard y boleto presencial

--- views/paginas/astrocamp.php
+++ views/paginas/astrocamp.php
@@ -1,19 +1,23 @@
-<main> 
-  <h2 class="astrocamp__heading"><?php echo $titulo;?></h2>
+<main>
+  <h2 class="astrocamp__heading">
+    <?php echo $titulo; ?>
+  </h2>
   <p class="astrocamp__descripcion">Conoce la conferencia más importante de LATAM</p>
 
   <div class="astrocamp__grid">
     <div class="astrocamp_imagen">
-      <picture>
-        <source srcset="build/img/sobre_astrocamp.avif" type="image/avif"> 
-        <source srcset="build/img/sobre_astrocamp.webp" type="image/webp"> 
-       <img loading="lazy" width="200" height="300" src="build/img/sobre_astrocamp.jpg" alt="Imagen AstroCamp">
-      </picture>
+      <video autoplay muted loop controls width="600" height="400">
+        <source src="build/video/video.mp4" type="video/mp4">
+        <source src="build/video/video.webp" type="video/webp">
+      </video>
     </div>
 
     <div class="astrocamp__contenido">
-      <p class="astrocamp__texto">¡Bienvenido al emocionante mundo de la astronomía! Nuestra plataforma es tu ventana al cosmos, donde explorarás los misterios del universo a través de conferencias de renombrados expertos. Únete a nosotros mientras desvelamos los secretos de las estrellas, planetas y galaxias.</p>
-      <p class="astrocamp__texto">Inspírate, aprende y conecta con apasionados del espacio en nuestras conferencias y eventos astronómicos. ¡Prepárate para un viaje inolvidable a través de las maravillas del universo!</p>
+      <p class="astrocamp__texto">¡Bienvenido al emocionante mundo de la astronomía! Nuestra plataforma es tu ventana al
+        cosmos, donde explorarás los misterios del universo a través de conferencias de renombrados expertos. Únete a
+        nosotros mientras desvelamos los secretos de las estrellas, planetas y galaxias.</p>
+      <p class="astrocamp__texto">Inspírate, aprende y conecta con apasionados del espacio en nuestras conferencias y
+        eventos astronómicos. ¡Prepárate para un viaje inolvidable a través de las maravillas del universo!</p>
     </div>
 
   </div>
